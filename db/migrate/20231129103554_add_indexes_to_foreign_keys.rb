2bdfdafa6e64bd23e502eb86c466a0ff529e737e
megagig <megagigsoftwaresolutions@gmail.com>
2023-11-29 11:39:30 +0100

    Edit the generated migration file

--- db/migrate/20231129103554_add_indexes_to_foreign_keys.rb
+++ db/migrate/20231129103554_add_indexes_to_foreign_keys.rb
@@ -1,4 +1,9 @@
 class AddIndexesToForeignKeys < ActiveRecord::Migration[7.1]
   def change
+    add_index :posts, :author_id
+    add_index :comments, :user_id
+    add_index :comments, :post_id
+    add_index :likes, :user_id
+    add_index :likes, :post_id
   end
 end
