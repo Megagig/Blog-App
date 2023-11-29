c015baeeb76e95147338fcf8d5dbf1cdc1b680d8
megagig <megagigsoftwaresolutions@gmail.com>
2023-11-29 11:10:13 +0100

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_095416) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "Name"
+    t.string "Photo"
+    t.text "Bio"
+    t.integer "PostCounter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
