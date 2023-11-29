3cd682ed1a318fd82211146e21a17989247f85d6
megagig <megagigsoftwaresolutions@gmail.com>
2023-11-29 20:21:02 +0100

    Fix linters bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
-  #The dependent: :destroy ensures that when a post is deleted, its
+  # The dependent: :destroy ensures that when a post is deleted, its
   # associated likes and comments will also be deleted.
   has_many :comments, foreign_key: 'posts_id', dependent: :destroy
   has_many :likes, dependent: :destroy
