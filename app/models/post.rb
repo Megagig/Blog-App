cd7f6d9cd0d09899b580c5eaa1df2dad4b4968fa
megagig <megagigsoftwaresolutions@gmail.com>
2023-11-29 18:23:42 +0100

    Create model classes and set up associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  belongs_to :user, foreign_key: 'author_id'
+  #The dependent: :destroy ensures that when a post is deleted, its
+  # associated likes and comments will also be deleted.
+  has_many :comments, foreign_key: 'posts_id', dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
