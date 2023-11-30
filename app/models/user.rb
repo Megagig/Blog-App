c0b966de55aaba6864a2cbdc7bfc6ad07993ee6b
megagig <megagigsoftwaresolutions@gmail.com>
2023-11-30 10:52:23 +0100

    Updated Posts to posts associated with the user instance

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   # The function retrieves the three most recent posts written
   # by the author.
   def three_most_recent_posts
-    Post.where(author_id: id).order(created_at: :desc).limit(3)
+    posts.order(created_at: :desc).limit(3)
   end
 end
