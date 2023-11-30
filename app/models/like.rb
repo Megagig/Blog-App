c32c625955fa3d87f6e51f2580c850b481dfb5f5
megagig <megagigsoftwaresolutions@gmail.com>
2023-11-30 11:05:22 +0100

    Update like counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,10 @@ class Like < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
   belongs_to :post, foreign_key: 'post_id'
 
+  after_save :update_post_likes_counter
+
+  private
+
   # The function `update_post_likes_counter` increments the `LikesCounter`
   # attribute of a post.
   def update_post_likes_counter
