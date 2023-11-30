cba5e2cc8247048b84e5274bf7df5414f3b2d7d6
megagig <megagigsoftwaresolutions@gmail.com>
2023-11-30 11:01:48 +0100

    Update post counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
-  # The dependent: :destroy ensures that when a post is deleted, its
-  # associated likes and comments will also be deleted.
   has_many :comments, foreign_key: 'posts_id', dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  # The function updates the PostCounter attribute of a user by counting
-  # the number of posts they have.
-  def update_posts_counter
-    user.update(PostCounter: user.posts.count)
-  end
+  after_save :update_posts_counter
 
-  # The function returns the five most recent comments in descending order
-  # of their creation time.
   def five_most_recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    user.update(PostCounter: user.posts.count)
   end
 end
