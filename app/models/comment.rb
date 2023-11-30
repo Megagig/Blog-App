200f7b4c42a62ff38971e2601ca184e3a767e2a3
megagig <megagigsoftwaresolutions@gmail.com>
2023-11-30 11:09:52 +0100

    Update comments counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
   belongs_to :post, foreign_key: 'post_id'
 
+  after_save :update_post_comments_counter
+
+  private
+
   # The function `update_post_comments_counter` increments the
   # `CommentsCounter` attribute of a post.
   def update_post_comments_counter
