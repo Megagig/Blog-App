1d19b7a68cbc1c91d180a1eb645281ffebef4623
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-01 12:06:13 +0100

    Fix linter error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe User, type: :model do
 
   it 'should return three most recent posts' do
     user = User.create(name: 'John Doe', post_counter: 0)
-    posts = (1..5).map { |i| Post.create(title: "Post #{i}", user: user, comments_counter: 0, likes_counter: 0) }
+    posts = (1..5).map { |i| Post.create(title: "Post #{i}", user:, comments_counter: 0, likes_counter: 0) }
     expect(user.three_most_recent_posts).to eq(posts.last(3).reverse)
   end
 end
