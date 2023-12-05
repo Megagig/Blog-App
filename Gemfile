6fe754dd970214be30896dd3076543beb6ceadca
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-05 08:15:29 +0100

    Fix linter error

--- Gemfile
+++ Gemfile
@@ -48,8 +48,8 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri windows]
   gem 'pg'
-  gem 'rspec-rails'
   gem 'rails-controller-testing'
+  gem 'rspec-rails'
 end
 
 group :development do
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
   # The index method is used to display all the posts for a given user.
-  # The index method will be called when a user navigates to the URL /users/:author_id/posts.
-  # The index method will be called when a user navigates to the URL /users/:author_id/posts.
+  # The index method will be called when a user navigates to the URL /users/:user_id/posts.
   def index
-    @user = User.find(params[:author_id])
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
-  # The index method is used to retrieve a specific post from the database and display it to the user.
+  # The show method is used to retrieve a specific post from the database and display it to the user.
   def show
-    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
   end
 end
