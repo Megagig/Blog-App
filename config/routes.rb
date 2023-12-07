4065b9d065ecb2dfc76e93a0d38355041140ebad
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-07 12:35:16 +0100

    Create post route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   root to: "users#index"
+  #Create a new post on behalf of the current_user
+  post "/posts", to: "posts#create"
+
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      #Create a new post on behalf of the current_user
+      post "/comments", to: "posts#create_comment", as: :post_comments
+
+      #Allow the current_user to add a like to a post
+      post "/likes", to: "posts#create_like", as: :post_likes
+    end
   end
 end
 
