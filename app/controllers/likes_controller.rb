17e97c25f34887e3cf329529b34897f986742957
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-07 14:51:41 +0100

    Add current_user method to get the first user

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,18 @@
 class LikesController < ApplicationController
-    def create_like
-      @post = Post.find(params[:id])
-      @like = current_user.likes.build(like_params.merge(posts_id: @post.id))
-      if @like.save
-        @like.update_post_likes_counter
-        redirect_to user_post_path(@post), notice: 'Post liked successfully.'
-      else
-        render :show
-      end
-    end
-
-    private
-
-    def like_params
-      params.require(:like).permit(:users_id, :posts_id)
+  def create_like
+    @post = Post.find(params[:id])
+    @like = current_user.likes.build(like_params.merge(posts_id: @post.id))
+    if @like.save
+      @like.update_post_likes_counter
+      redirect_to user_post_path(@post), notice: 'Post liked successfully.'
+    else
+      render :show
     end
   end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:users_id, :posts_id)
+  end
+end
