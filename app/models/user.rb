4f12560c0913f14f4055104c134b11ab0976e406
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-13 14:12:50 +0100

    Add sign up permissions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  #devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+  # devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id', counter_cache: true
   has_many :comments
   has_many :likes
