4a3bc146956fdd4e949aacfb24620bbafefface7
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-13 14:45:14 +0100

    modify success and error tags

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id', counter_cache: true
   has_many :comments
   has_many :likes
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true # This is validating that the `Name`
   # attribute of the `User` model is present, meaning it cannot be blank or nil.
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   # is adding a validation to the `User` model. It ensures that the `PostCounter` attribute is a
   # numerical value and that it is greater than or equal to 0. This means that the `PostCounter`
   # attribute cannot be a non-integer value or a negative number.
