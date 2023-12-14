2641c02738d7737dc99a40917358d9b881f70271
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-12-14 09:45:46 +0500

    Define Ability Rules with CanCan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,14 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # Guest user
+
+    if user.role == 'admin'
+      can :manage, :all # Admins can manage all resources
+    else
+      can :manage, Post, user_id: user.id
+      can :manage, Comment, user_id: user.id
+    end
+  end
 end
